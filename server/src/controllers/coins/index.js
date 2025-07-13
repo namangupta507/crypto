9adbc61bbeff04cf70fcc0b2cac01911fc11c613
Naman Gupta <naman.gupta507@gmail.com>
2025-07-13 11:37:36 +0530

    fix cors issue

--- server/src/controllers/coins/index.js
+++ server/src/controllers/coins/index.js
@@ -2,10 +2,22 @@ import axios from "axios";
 import currentCoinData from "../../models/cuurentCoinData.js";
 
 const getCoins = async (req, res) => {
+  let coinCache = null;
+  let lastFetch = 0;
+  const now = Date.now();
+  const CACHE_TTL = 60 * 1000;
   try {
     const page = req.query.page || 1;
     const per_page = req.query.per_page || 5;
     const vs_currency = req.query.vs_currency;
+
+    if (coinCache && now - lastFetch < CACHE_TTL) {
+      return res.status(200).json({
+        message: "coins list fetched successful (from cache)",
+        status: true,
+        data: coinCache,
+      });
+    }
 
     const coinsList = await axios.get(process.env.GET_COINS_ENDPOINT, {
       params: {
@@ -25,11 +37,10 @@ const getCoins = async (req, res) => {
         percentChange24h: coin.price_change_percentage_24h,
       };
 
-      await currentCoinData.findOneAndUpdate(
-        { coinId: coin.id },
-        updateData,
-        { upsert: true, new: true }
-      );
+      await currentCoinData.findOneAndUpdate({ coinId: coin.id }, updateData, {
+        upsert: true,
+        new: true,
+      });
     }
 
     return res.status(200).send({
@@ -39,11 +50,11 @@ const getCoins = async (req, res) => {
         list: coinsList.data,
         page: page,
         per_page: per_page,
-        hasMore: coinsList.data.length === per_page
+        hasMore: coinsList.data.length === per_page,
       },
     });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return res
       .status(500)
       .send({ message: error.message, status: false, error: error });
